236ffb1284b2e53fcc86d2a4a2d3733fdb4f9d5d
jingkaimori <wuyiheng6@163.com>
2020-06-14 15:33:58 +0800

    修改程序逻辑，配置文件中可配置模式

--- fliter.js
+++ fliter.js
@@ -1,14 +1,19 @@
 "use strict";
 /**
- * 根据配置文件读取期刊文件，将期刊拆分为条目，输出至result.json文件。
+ * 从配置文件中读取期刊文件的目录和元数据模式，读取期刊文件，将期刊拆分为条目，输出至result.json文件。
  * 已经迁移至deno。
  * 
  * deno的1.1版本修复了无法使用远程ts模块的bug。
  */
-import {readFileStrSync,writeJson,walk} from "https://deno.land/x/std/fs/mod.ts";
-import {join} from "https://deno.land/x/std/path/mod.ts";
-//var config;
-import {readConfig,config} from "./configManager.js";
+import { readFileStrSync, writeJson, walk } from "https://deno.land/x/std/fs/mod.ts";
+import { join } from "https://deno.land/x/std/path/mod.ts";
+import { readConfig, config } from "./configManager.js";
+
+function debugOutput(mesg) {
+	if(config.debug){
+		console.log(mesg);
+	}
+}
 /**
  * 将期刊拆分为条目。
  * @param {String} data 期刊的markdown格式文本
@@ -16,13 +21,16 @@ import {readConfig,config} from "./configManager.js";
  */
 function parser(data, issueNum) {
 	data = data.replace(/<br>/img, '\n');
+	let num = 0;
 	let res = [];
 	let tag = "";
 	let lines = data.split(/\n{2,}/);
 	function Item() {
+		num++;
 		this.content = [];
 		this.images = [];
 		this.issueNum = issueNum;
+		this.id = issueNum + "-" + num;
 	}
 	Item.prototype.push = function () {
 		this.tag = tag;
@@ -30,88 +38,60 @@ function parser(data, issueNum) {
 	};
 	let item = new Item();
 
-	let hasContent = false;
-	/**
-	 * 
-	 * @param {string} str 
-	 * @param {RegExp} regex 
-	 * @param {Function} func 
-	 */
-	function findMetadata(str, regex, func) {
-		return str.replace(regex, function () {
-			//截取参数列表第一项以后的部分。
-			var args = Array.prototype.slice.call(arguments, 1);
-			func.apply(this, args);
-			return '';
-		});
-	}
+	let istitle = true;
 	for (var i in lines) {
-
+		
 		let curline = lines[i];
-		if (curline.match(/^\d+、|#./)) {
-			if (hasContent) {
+		if (curline.match(/^\d+、|#+./)) {
+			if (!istitle) {
 				item.push();
 			}
-			hasContent = false;
 			item = new Item();
-			let titleline = curline.match(/^\d+、(\[(.*)\]\((.*)\))?(（(.*)）)?/);
-			if (titleline) {
-				if (titleline[2]) {
-					item.title = titleline[2];
-				} else if (curline.match(/^\d+、(.*)/)[1]) {
-					item.title = curline.match(/^\d+、(.*)/)[1];
-				} else {
-					item.title = '无题';
-				}
-
-				if (titleline[3]) {
-					item.reference = titleline[3];
-				}
-				item.lang = titleline[5] || '不明';
-			}
-
 			let tagline = curline.match(/^#+ (.+)$/);
 			if (tagline) {
 				tag = tagline[1];
 			}
-		} else {
-			curline = findMetadata(curline, /!\[.*\]\((.*)\)/,
-				function (p1) {
-					item.images.push(p1);
-				}
-			);
-			curline = findMetadata(curline,
-				/（@\[(.*)\]\(([^)]+)\) 投稿）/,
-				function (p1, p2) {
-					item.recommender = p1;
-					item.issue = p2;
-				}
-			);
-			curline = findMetadata(curline,
-				/^-- *\[(.*)\]\((.*)\)$/,
-				function (p1, p2) {
-					item.title = p1;
-					item.reference = p2;
-				}
-			);
-			curline = findMetadata(curline,
-				/^-- *(.+)$/,
-				function (p1) {
-					item.reference = p1;
-				}
-			);
-			if (curline.length > 0) {
-				item.content.push(curline);
-			}
-			hasContent = true;
+			curline = curline.replace(/^\d+、|#+./, '');
+			istitle = true;
+		}else{
+			istitle = false;
 		}
 
+		for (i in config.rules) {
+			const currule= config.rules[i];
+			let arg = RegExp(currule.match).exec(curline);
+			if (arg && (currule.title == istitle)) {
+				debugOutput("Entry "+item.id+" Matches Rule "+i+" . Title :" + istitle);
+				switch (currule.type) {
+					case "attribute":
+						for (let j in currule.attribute) {
+							item[currule.attribute[j]] = arg[j];
+						}
+						break;
+					case "list":
+						/* console.log("debug begin:\n\t"+
+							item[currule.list] + "\n\t" +
+							item[currule.list].concat(arg.slice(1)) +"\n" +
+							"debug end"
+						); */
+						item[currule.list] =
+							item[currule.list].concat(arg.slice(1));
+						break;
+				}
+				if (currule.remove) {
+					curline = curline.replace(arg[0], '');
+				}
+			}
+		}
+		if (curline.length > 0) {
+			item.content.push(curline);
+		}
 	}
 	return res;
 }
 readConfig().then(
-	async ()=>{
-		console.log("Config!");
+	async () => {
+		console.log("Started!");
 		let res = [];
 		// console.log('***' + JSON.stringify(config));
 
@@ -119,32 +99,36 @@ readConfig().then(
 		 * 使用walk函数来遍历目录，检索期刊文章。 
 		 * 读完所有文件后写入result.json
 		 */
-		for await (const entry  of walk(config.weeklyResponsitry,{match:[/issue-(\d+)/]})){
-			try{
-				console.log("ping!");
+		for await (const entry of walk(config.weeklyResponsitry, { match: [/issue-(\d+)/] })) {
+			try {
+				//console.log("ping!");
 
 				let [, issueNum] = entry.path.match(/issue-(\d+)/);
-				let data=readFileStrSync(entry.path);
-				let pRes=parser(data, parseInt(issueNum));
-				
+				let data = readFileStrSync(entry.path);
+				let pRes = parser(data, parseInt(issueNum));
+
 				res = res.concat(pRes);
-			}catch (err){
-				console.error(new Error(err + "is not found or invalid"));
+			} catch (err) {
+				if (typeof err == SyntaxError) {
+					console.error(new Error(err + "is not found or invalid"));
+				} else {
+					console.error(err);
+				}
 			}
 		}
 		let resFile = join(config.output || 'result.json');
-		console.log( "modifying " + resFile);
-		return writeJson(resFile, res,  {spaces:4});
+		debugOutput("modifying " + resFile);
+		return writeJson(resFile, res, { spaces: 4 });
 	}
 ).then(
-	()=>{
-		console.log( "resFile has been modified");
+	() => {
+		debugOutput("resFile has been modified");
 	},
-	(err)=>{
+	(err) => {
 		console.log("resFile cannot be wrote");
-	} 
+	}
 ).catch(
-	(err)=>{
-		console.log("Error!\n\t"+err);
-	} 
+	(err) => {
+		console.log("Error!\n\t" + err);
+	}
 );
